1d60b25003c3fb5ab8dc397746b4a65b41189fc8
Floortje <florian.tjeertes@ziggo.nl>
2024-08-14 01:22:18 +0200

    chore: Add input handling for Pacman movement

--- Assets/pacman.cs
+++ Assets/pacman.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class pacman : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 direction = value.Get<Vector2>();
+        SetDirection(direction);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        if (direction == Vector2.up)
+        {
+            Debug.Log("Pacman is moving up");
+        }
+        else if (direction == Vector2.down)
+        {
+            Debug.Log("Pacman is moving down");
+        }
+        else if (direction == Vector2.left)
+        {
+            Debug.Log("Pacman is moving left");
+        }
+        else if (direction == Vector2.right)
+        {
+            Debug.Log("Pacman is moving right");
+        }
+
     }
 }
